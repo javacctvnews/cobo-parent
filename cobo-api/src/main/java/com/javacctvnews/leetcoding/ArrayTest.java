02cbe08a0e903123de49bdddb1536dd508297814
cyh <cyh@qq.com>
2018-05-29 22:08:53 +0800

    添加测试类

---
+++ cobo-api/src/main/java/com/javacctvnews/leetcoding/ArrayTest.java
@@ -0,0 +1,13 @@
+package com.javacctvnews.leetcoding;
+
+/**
+ * Author：javacctvnews
+ * Create：2018/5/29 21:12
+ * Desc：
+ */
+
+public class ArrayTest {
+
+
+
+}
--- cobo-api/src/test/java/com/javacctvnews/AppTest.java
+++ cobo-api/src/test/java/com/javacctvnews/AppTest.java
@@ -1,38 +1,29 @@
 package com.javacctvnews;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest{
+
+    @Test
+    public void test001(){
+
+        int[] arr = {1,2,2,3,1,3,2};
+
+        for (int i=0;i<arr.length;i++){
+
+        }
+
+    }
+    @Test
+    public void test002(){
+
+        String str = "abcdefg";
+
+
+
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
